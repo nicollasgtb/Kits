d312d260923b290d9bc6ce809e44207249811cbf
nicollasgtb <nicolas2006.capela@gmail.com>
2025-11-05 16:26:51 -0300

    Added a new method to remove a specific player from the cache and restructured the code.

--- src/main/java/br/ynicollas/kits/cache/CooldownsCache.java
+++ src/main/java/br/ynicollas/kits/cache/CooldownsCache.java
@@ -1,6 +1,7 @@
 package br.ynicollas.kits.cache;
 
 import org.bukkit.entity.Player;
+
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -8,6 +9,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public class CooldownsCache {
 
     private final Map<String, Map<String, Long>> cooldownsCache = new ConcurrentHashMap<>();
+
+    public boolean hasCooldown(Player player, String id) {
+        Map<String, Long> kitMap = cooldownsCache.get(player.getName());
+
+        return kitMap != null && kitMap.getOrDefault(id, 0L) > System.currentTimeMillis();
+    }
+
+    public long getCooldown(Player player, String id) {
+        return cooldownsCache.getOrDefault(player.getName(), Collections.emptyMap()).getOrDefault(id, 0L);
+    }
 
     public void addCooldown(Player player, String id, long expireTime) {
         cooldownsCache.computeIfAbsent(player.getName(), key -> new ConcurrentHashMap<>())
@@ -21,17 +32,11 @@ public class CooldownsCache {
         });
     }
 
+    public void removePlayer(Player player) {
+        cooldownsCache.remove(player.getName());
+    }
+
     public void removeCooldownsForKit(String id) {
         cooldownsCache.values().forEach(kitMap -> kitMap.remove(id));
-    }
-
-    public boolean hasCooldown(Player player, String id) {
-        Map<String, Long> kitMap = cooldownsCache.get(player.getName());
-
-        return kitMap != null && kitMap.getOrDefault(id, 0L) > System.currentTimeMillis();
-    }
-
-    public long getCooldown(Player player, String id) {
-        return cooldownsCache.getOrDefault(player.getName(), Collections.emptyMap()).getOrDefault(id, 0L);
     }
 }
